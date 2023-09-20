465733e3ee9816a5cf1cd2e6ad0d4f32fb9d75ec
Gabriel Ocampos <gabriel1234511@hotmail.com>
2023-09-20 00:29:34 -0300

    routing

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-import express, { Router } from 'express';
+import express from 'express';
+import ProductRouter from './Router/ProductRouter.js'
+import { __dirname } from "./utils.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/Public'))
 
 
-app.use("api/products", )
-app.use("api/cart", )
-
+app.use("/api/products", ProductRouter)
 
 
 app.listen(8080, () => {
