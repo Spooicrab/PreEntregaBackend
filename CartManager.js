d57710b34d5ee02e7aa04d8b2fe4ae606e7b42dc
Gabriel Ocampos <gabriel1234511@hotmail.com>
2023-09-20 12:45:48 -0300

    p

--- CartManager.js
+++ CartManager.js
@@ -2,10 +2,23 @@ import fs from 'fs'
 
 class GestorCarrito {
 
+    constructor(path) {
+        this.path = path;
+    }
 
+    async GetCart() {
+        try {
+            const ArchivoCarrito = await fs.promises.readFile(this.path, "utf-8");
+            ListaCarritos = Json.parse(ArchivoCarrito);
+            return fs.existsSync(this.path) ? ListaCarritos : []
+        } catch (error) {
+            return error;
+        }
+    }
 
 
 
 }
 
-export const CartManager = new GestorCarrito(Cart.json)
+test ()
+export const CartManager = new GestorCarrito("Cart.json");
